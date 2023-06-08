90da3a3cee21b3e2791d870b1b067f6557bb4de0
JacobSandbox <129905424+JacobSandbox@users.noreply.github.com>
2023-06-07 22:33:27 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,10 +6,40 @@
 // Home page
 //////////////////////
 
-function gotoCard ( card ) {
-    switch ( card ) {
-        case "About":
-            window.scroll(0, 600);
-            break;
-    }
+
+//////////////////////
+// Services page
+//////////////////////
+
+function test() {
+    const div = document.createElement("div");
+    document.getElementById("content").append(div);
+}
+
+function load() {
+    generateCard("lawnmower", true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+    generateCard("trimming", false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+    generateCard("flowers", true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+    generateCard("rocks", false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+}
+
+function generateCard ( img, dir, txt ) {
+    // Generate div
+    const card = document.createElement("div");
+    card.className = "card";
+    // Add image
+    const pic = document.createElement("img");
+    pic.src = "../../assets/images/" + img + ".png";
+    // Set image style
+    pic.style = (dir) ? "float:left" : "float:right";
+    card.appendChild(pic);
+    // Add text
+    const desc = document.createElement("p");
+    desc.innerHTML = txt;
+    desc.className = "desc";
+    card.appendChild(desc);
+    // Get main content div
+    const main = document.getElementById("content");
+    // Append new div to content
+    main.appendChild(card);
 }
